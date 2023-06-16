78c5256c170e22394f28c3425e6ff7c6cc0c81aa
Atulaya123 <arai2_be20@thapar.edu>
2023-06-16 12:37:04 +0530

    Update layout.tsx
    https://chat.openai.com/share/e236eff3-204d-40de-aa05-0e3612781ca8
    
    updated fonts
    added some styling to children and body tag
    used cn utility function which is smart enough to help combine tailwind together
    e.g.  if we pass my-2 mx-2 ->it combines them into m-2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import '@/styles/globals.css'
+import {cn} from '@/lib/utils'
+import {Inter} from 'next/font/google'
+
 
 export const metadata = {
   title: 'Breadit',
   description: 'A Reddit clone built with Next.js and TypeScript.',
 }
+
+const inter=Inter({subsets:['latin']});
 
 export default function RootLayout({
   children,
@@ -11,8 +16,17 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html lang='en'
+    className={cn(
+      'bg-white text-slate-900 antialiased light',
+      inter.className
+    )}>
+      <body className='min-h-screen pt-2 bg-slate-50 antialiased'>
+        {/* <Navbar/> */}
+        <div className='container' max-w-7xl max-auto h-full pt-12>
+        {children}
+        </div>
+        </body>
     </html>
   )
 }
